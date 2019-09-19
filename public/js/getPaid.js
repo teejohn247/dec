70d61b84ac5ddee522d5b6925bd9af3ad5f01480
teejohn247 <teejohn247@gmail.com>
2019-09-19 01:34:06 +0100

    Added the payment functionality

---
+++ public/js/getPaid.js
@@ -0,0 +1,53 @@
+const getPaid = () => {  
+
+    fetch(
+     "http://localhost:3000/employees?status=PAID", {
+      method: 'GET',
+      headers: {
+            "Content-Type": 'application/json', 
+        },
+    })
+    .then(res => res.json())
+    .then(data => {
+        console.log(data)
+        const res = data;
+        if (res.length > 0) {
+          res.map((employees) => {
+            const { id, email, firstName, lastName,
+                address, mobile, status, salary,
+               maritalStatus } = employees
+    
+    const tab = document.querySelector('.tabb');
+    const theadd = document.createElement('thead');
+    const trr = document.createElement('tr');
+    const thh = document.createElement('th');
+    const thh1 = document.createElement('th');
+    const thh2 = document.createElement('th');
+    const thh3 = document.createElement('th');
+    const thh4 = document.createElement('th');
+    const thh5 = document.createElement('th');
+    const thh6 = document.createElement('th');
+    const thh7 = document.createElement('th');
+    
+    
+    
+    trr.append(thh1,thh2,thh3,thh4,thh5,thh6,thh7);
+    theadd.appendChild(trr);
+    tab.appendChild(theadd);
+    
+    thh1.textContent= `${firstName}`;
+    thh2.textContent= `${lastName}`;
+    thh3.textContent= `${email}`;
+    thh4.textContent= `${address}`;
+    thh5.textContent= `${status}`;
+    thh6.textContent= `${salary}`;
+    thh7.textContent= `${mobile}`;
+    })
+    }
+    })
+    .catch ((error) => {
+        console.log(error.message);
+    });
+    };
+    getPaid()
+    
--- public/js/specific.js
+++ public/js/specific.js
@@ -1,30 +1,93 @@
-const specific = (id) => {
-    fetch(
-     `http://localhost:3000/employees/${id}`, {
-      method: 'GET',
-      headers: {
-        "Content-Type": 'application/json', 
-          "Authorization": token
-        }
-        
-    })
-        .then(res => res.json())
-        .then(data => {
-            console.log(data)
-           if (data.length > 0) {
-      res.map((employees) => {
-        const { email, firstName, lastName,
-            address, mobile, status,
-           maritalStatus } = employees
-           const example = document.querySelector('.examp');
-           const h2 = document.createElement('h2')
-           example.appendChild('h2');
-           h2.textContent =`${maritalStatus}`
+const payAll = (id) => {
+  const storage = JSON.parse(localStorage.getItem('Employee'));
+   
+    
+        fetch(
+         `http://localhost:3000/employees/${id}`, {
+          method: 'PUT',
+          headers: {
+            "Content-Type": 'application/json', 
+            },
+            body: JSON.stringify({ email:storage.email, firstName: storage.firstName, lastName: storage.lastName,
+              address:storage.address, mobile: storage.mobile, status: 'PAID', salary:storage.salary,
+             maritalStatus:storage.maritalStatus})
         })
-    }
-    })
-    .catch ((error) => {
-        console.log(error.message);
-    });
-    event.preventDefault();
+  
+            .then(res => res.json())
+            .then(data => {
+    console.log(data)
+                
+         
+            })
+            .catch(error => console.log(error.message));
+        event.preventDefault();
     };
+    
+  
+  const specific = (id) => {
+      fetch(
+       `http://localhost:3000/employees/${id}`, {
+        method: 'GET',
+        headers: {
+          "Content-Type": 'application/json', 
+            "Authorization": token
+          }
+          
+      })
+          .then(res => res.json())
+          .then(data => {
+              console.log(data)
+             if (data.length > 0) {
+        res.map((employees) => {
+          const {id, email, firstName, lastName,
+              address, mobile, status,
+             maritalStatus } = employees
+             const example = document.querySelector('.examp');
+             const h2 = document.createElement('h2')
+             example.appendChild('h2');
+             h2.textContent =`${maritalStatus}`
+          })
+      }
+      })
+      .catch ((error) => {
+          console.log(error.message);
+      });
+      event.preventDefault();
+      };
+  
+      const populate = () => {  
+    
+        const storage = JSON.parse(localStorage.getItem('specific'));
+        const id = storage.id;
+        console.log(storage); 
+        const jumbo = document.querySelector('.jumbotron');
+        const h = document.createElement('h1');
+        const p1 = document.createElement('h3');
+        const p2 = document.createElement('h3');
+        const p3 = document.createElement('h3');
+        const p4 = document.createElement('h3');
+        const p5 = document.createElement('h3');
+        const p6 = document.createElement('h3');
+  
+        const btnn = document.createElement('a');
+        const btnn2 = document.createElement('a');
+  
+  
+  
+        jumbo.append(h,p1,p2,p3,p4,p5,p6,btnn,btnn2);
+        h.textContent =`${storage.firstName} ${storage.lastName}`;
+        p1.textContent =`${storage.maritalStatus}`;
+        p2.textContent =`${storage.mobile}`;
+        p3.textContent =`${storage.status}`;
+        p4.textContent =`${storage.salary}`;
+        p5.textContent =`${storage.address}`;
+        p6.textContent =`${storage.email}`;
+  
+  
+        btnn.textContent =`Learn more`;
+        btnn.innerHTML = `<a href="./editForm.html"><button class="btn btn-primary btn-lg">Edit</button></a>`
+        btnn.setAttribute('class', 'btn btn-primary btn-lg');
+        btnn2.innerHTML = `<button class="btn btn-primary btn-lg" onclick = "payAll(${id})">pay</button>`
+        btnn2.setAttribute('class', 'btn btn-primary btn-lg');
+      };
+      populate();
